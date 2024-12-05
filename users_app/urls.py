e294e9793909a0fd4c08b1f9e8069ee6a48ae7c4
Arifuzzaman Akash <mdarifuzzaman982k@gmail.com>
2024-12-05 23:06:29 +0600

    Login & Logout

--- users_app/urls.py
+++ users_app/urls.py
@@ -4,4 +4,6 @@ from .import views
 urlpatterns = [
     path("profile/", views.profile_page, name='profile_page'),
     path("register/", views.register, name='register'),
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
--- users_app/views.py
+++ users_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def profile_page(request):
@@ -14,3 +16,21 @@ def register(request):
             return redirect("home_page")
         
     return render(request, 'users_app/register.html', {'form': form})
+
+def login_view(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home_page')
+        else:
+            form = AuthenticationForm(request.POST)
+            return render(request, 'users_app/login.html', {'form': form})
+    return render(request, 'users_app/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home_page')
