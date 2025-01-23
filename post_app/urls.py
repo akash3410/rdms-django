1013448456582db011afba6bc0865d83ca8b4fa9
akash3410 <mdarifuzzaman982k@gmail.com>
2025-01-23 14:22:59 +0600

    Edit & Delete Post

--- post_app/urls.py
+++ post_app/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('create/', views.create_post, name='create_post'),
+    path('edit/<int:blog_id>', views.edit_post, name='edit_post'),
+    path('delete/<int:blog_id>', views.delete_post, name='delete_post'),
 ]
--- post_app/views.py
+++ post_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from .forms import BlogForm
 from django.contrib.auth.decorators import login_required
+from .models import Blog
 
 # Create your views here.
 @login_required
@@ -16,3 +17,21 @@ def create_post(request):
     else:
         form = BlogForm()
     return render(request, 'post_app/create_post.html', {'form':form})
+
+@login_required
+def edit_post(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id, user=request.user)
+    if request.method == "POST":
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_page')
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'post_app/edit_post.html', {'form': form})
+
+@login_required
+def delete_post(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id, user=request.user)
+    blog.delete()
+    return redirect('profile_page')
